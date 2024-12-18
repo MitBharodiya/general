26536df2c22627e3b2ef632ef75289480124316f
MitBharodiya <mit.bharodiya@braincuber.com>
2024-12-18 07:16:37 +0000

    ADD map

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
 const express = require("express");
-const app = express();
 const http = require("http");
 const path = require("path");
-const socketio = require('socket.io');
-const server =http.createServer(app);
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
 const io = socketio(server);
 
-app.set("view engin","ejs");
-app.set(express.static(path.join(__dirname,"public")))
+// Set EJS as the view engine
+app.set("view engine", "ejs");
 
-io.on("connection",function(socket){
-    console.log("Connected");
-})
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/demo",(req,res)=>{
-    console.log("its home page");    
-    res.send("its home page");    
+// Handle the root route and render the index.ejs view
+app.get("/", (req, res) => {
+    res.render("index"); // This will render views/index.ejs
 });
 
+// Handle Socket.io connections
+io.on("connection", (socket) => {
+    console.log("A user connected");
 
-server.listen(3000,()=>{
-    console.log("app is listen on port 3000");
-})
+    // Listen for 'send-location' event and log the location data
+    socket.on("send-location", (data) => {
+        console.log("Received location:", data);
+
+        // Optionally, broadcast this data to all connected clients
+        io.emit("location-update", data);
+    });
+
+    // Handle disconnections
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+    });
+});
+
+// Start the server on port 3000
+server.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
